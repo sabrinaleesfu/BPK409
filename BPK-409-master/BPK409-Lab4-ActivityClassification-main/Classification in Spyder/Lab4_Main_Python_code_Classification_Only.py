7aba3d8e99aaf3c8ea6a0f12244882747b6e6e48
sabrinaleesfu <127564777+sabrinaleesfu@users.noreply.github.com>
2025-03-06 13:07:38 -0800

    Add files via upload

---
+++ BPK-409-master/BPK409-Lab4-ActivityClassification-main/Classification in Spyder/Lab4_Main_Python_code_Classification_Only.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 24 12:24:18 2024
+
+@author: Milad Hafezi
+"""
+
+
+import Classification_Function as l4CF
+
+
+l4CF.LabelData('Classification_Test_Data.txt')
+
+
